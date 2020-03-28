83c8ccde91a99db45879e4011b78bf70e00c4345
Tomas Baca <klaxalk@gmail.com>
2020-03-28 13:38:43 +0100

    updated tracker diag

--- src/automatic_start.cpp
+++ src/automatic_start.cpp
@@ -387,7 +387,8 @@ void AutomaticStart::timerMain([[maybe_unused]] const ros::TimerEvent& event) {
       std::scoped_lock lock(mutex_control_manager_diagnostics_);
 
       // if takeoff finished
-      if (control_manager_diagnostics.active_tracker == "MpcTracker" && !control_manager_diagnostics.tracker_status.moving_reference) {
+      if (control_manager_diagnostics.active_tracker != "NullTracker" && control_manager_diagnostics.active_tracker != "LandoffTracker" &&
+          !control_manager_diagnostics.tracker_status.have_goal) {
 
         ROS_INFO_THROTTLE(1.0, "[AutomaticStart]: takeoff finished");
 
