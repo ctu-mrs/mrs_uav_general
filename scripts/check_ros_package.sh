a707262b81738fcbfca79486f7bd9b66e8162dce
Dan Hert <hertdani@fel.cvut.cz>
2022-02-22 10:50:30 +0100

    updated check_ros_package scirpt

--- scripts/check_ros_package.sh
+++ scripts/check_ros_package.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 1; done
+  return 0
+}
+
 if [ "$#" -ne 1 ]; then
     echo "Skipping required package check, no list of packages provided!"
     exit 0
@@ -14,20 +21,42 @@ result=0
 RED='\e[31m'
 GREEN='\e[32m'
 NC='\e[39m' # No Color
+IFS=' - '
+declare meta_array=()
 
 while read line; do
-  VAR=$(rospack list-names | grep $line)
-  echo -e "checking: $line ... \c"
+
+  read -ra str_arr <<< "$line"
+
+  VAR=$(rospack list-names | grep ${str_arr[0]})
+  echo -e "checking: ${str_arr[0]} ... \c"
   if [ -z "${VAR}" ]; then
-    echo -e "${RED}missing${NC}"
+    if [[ "${#str_arr[@]}" -eq 2 ]]; then
+      echo -e "${RED}missing${NC} (part of metapackage ${str_arr[1]})"
+        containsElement "${str_arr[1]}" "${meta_array[@]}"
+        if [[ "$?" -eq 0 ]]; then
+          meta_array+=("${str_arr[1]}")
+        fi
+    else
+      echo -e "${RED}missing${NC}"
+    fi
+
     result=1
   else
     echo -e "${GREEN}found${NC}"
   fi
 done < $1
 
+
 if [[ $result -ne 0 ]]; then
-  echo -e "${RED}Some required packages were not found, the simulation will probably not work correctly.${NC}"
+
+  echo -e "\n${RED}Some required packages were not found, the simulation will probably not work correctly.${NC}"
+
+  if [[ "${#meta_array[@]}" -ne 0 ]]; then
+    echo -e "You are missing the following metapackages:"
+    echo "${meta_array[*]}"
+  fi
+  echo -e ""
 
   default=n
   { read -t 10 -n 2 -p $'Proceed to run the simulation? [y/n] (default: '"$default"$')' resp || resp=$default ; }
